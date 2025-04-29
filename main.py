e284af166b77f72027f2ddbb120c3e89bec0326f
Ilia <ilia.moroz@epiroc.com>
2025-04-29 13:47:17 -0700

    CSV output added

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import endaq.device
 import endaq.ide
 import endaq
+import pandas as pd
+import numpy as np
 import time
 import os
+import sys
 
-device = endaq.device.getDevices()[0]
-print(device.path)
+try:
+    device = endaq.device.getDevices()[0]
+except IndexError:
+    print("no device found")
+    sys.exit()
 path = os.path.join(device.path, "DATA\\RECORD")
-print(path)
 
 interface = endaq.device.command_interfaces.CommandInterface(device)
-
-print(interface.canRecord)
-
-print(device.channels)
 
 device.startRecording()
 
@@ -25,20 +26,38 @@ while len(endaq.device.getDevices()) == 0:
 
 print(endaq.device.getDevices())
 
-newfile = os.listdir(path)[-1] 
+newfile = os.listdir(path)[-1]
 newfile = os.path.join(path, newfile)
 
 doc = endaq.ide.get_doc(newfile)
 
-# endaq.plot.utilities.set_theme('endaq')
-
 # Get All Data
-data = {doc.channels[ch].name: endaq.ide.to_pandas(doc.channels[ch], time_mode='datetime') for ch in doc.channels}
+data = {doc.channels[ch].name : endaq.ide.to_pandas(doc.channels[ch], time_mode='seconds') for ch in doc.channels}
+
+#frame = pd.DataFrame(data, index = [0])
+
+frames = [endaq.ide.to_pandas(doc.channels[ch], time_mode='seconds') for ch in doc.channels]
+
+
+files = [f for f in os.listdir('.') if os.path.isfile(f)]
+
+try:
+    frames[0].to_csv("25g.csv")
+except PermissionError:
+    print("file is opened. Cannot override the file")
+    sys.exit()
+
+try:
+    frames[1].to_csv("40g.csv")
+except PermissionError:
+    print("file is opened. Cannot override the file")
+    sys.exit()
 
 # Get Dashboard of All Channels
 dashboard = endaq.plot.dashboards.rolling_enveloped_dashboard(
     data,
-    num_rows=1,
-    num_cols=None
+    num_rows=None,
+    num_cols=1
 )
 dashboard.show()
+
